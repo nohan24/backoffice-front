3ce389192a9a7f31e9ff746bd542e016f7396f11
nohan24 <nohanrazafinjato@gmail.com>
2024-01-12 17:25:25 +0300

    annonce attente card

--- src/components/AnnonceCard.js
+++ src/components/AnnonceCard.js
@@ -1,7 +1,71 @@
+import './annoncecard.css'
+import AspectRatio from '@mui/joy/AspectRatio';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import Typography from '@mui/joy/Typography';
+import Golf from '../img/golf.jpg'
+import {Button, Stack} from "@mui/joy";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 export default function AnnonceCard(){
     return(
         <>
-            card
+            <Card className="card-style">
+                <div>
+                    <Typography level="title-lg"><p style={{
+                        margin: '5px 0',
+                        width: '100%',
+                        whiteSpace: 'nowrap',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis'
+                    }}>Volkswagen Golf 7 2.0 TDI</p></Typography>
+                    <Typography level="body-sm">Publié le : 2023/12/12</Typography>
+
+                </div>
+                <AspectRatio minHeight="400px" maxHeight="400px">
+                    <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+                        <SwiperSlide>
+                            <img src={Golf} alt=""/>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src={Golf} alt=""/>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src={Golf} alt=""/>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src={Golf} alt=""/>
+                        </SwiperSlide>
+
+                    </Swiper>
+                </AspectRatio>
+                <CardContent orientation="horizontal">
+                    <Stack direction="row"
+                           justifyContent="space-between"
+                           alignItems="center"
+                            sx={{width: 100 + '%'}}>
+                        <div>
+                            <Typography level="body-sm">Prix de la voiture :</Typography>
+                            <Typography fontSize="lg" fontWeight="lg">
+                                52.000.000 MGA
+                            </Typography>
+                        </div>
+                        <Stack>
+                            <Button
+                                variant="solid"
+                                size="md"
+                                color="primary"
+                                aria-label="Explore Bahamas Islands"
+                            >
+                                Explore
+                            </Button>
+                        </Stack>
+                    </Stack>
+                </CardContent>
+            </Card>
         </>
     )
 }
--- src/pages/Annonce.js
+++ src/pages/Annonce.js
@@ -1,8 +1,26 @@
+import Grid from "@mui/joy/Grid";
+import {Box} from "@mui/joy";
 import AnnonceCard from '../components/AnnonceCard'
 export default function Annonce(){
     return(
         <>
-            <AnnonceCard/>
+            <Box>
+                <Grid container spacing={2} sx={{ flexGrow: 1 }}>
+                    <Grid xs={12} sm={4} md={6}>
+                        <AnnonceCard/>
+                    </Grid>
+
+                    <Grid xs={12} sm={4} md={6}>
+                        <AnnonceCard/>
+                    </Grid>
+
+                    <Grid xs={12} sm={4} md={6}>
+                        <AnnonceCard/>
+                    </Grid>
+
+
+                </Grid>
+            </Box>
         </>
     )
 }
