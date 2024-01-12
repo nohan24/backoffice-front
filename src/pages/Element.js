5ad736c0b6ff67d767938eb43475a8631bdca7a3
nohan24 <nohanrazafinjato@gmail.com>
2024-01-12 13:08:38 +0300

    fix url

--- src/pages/Element.js
+++ src/pages/Element.js
@@ -7,22 +7,22 @@ export default function Element(){
     return(
         <>
             <Stack   direction={{ xs: 'column', sm: 'row' }} spacing={3/2}>
-                <Link to="/myCar/elements/marque">
+                <Link to="/home/elements/marque">
                     <Button className="custom-btn" startDecorator={<DirectionsCar />}>Marque de voiture</Button>
                 </Link>
-                <Link to="/myCar/elements/modele">
+                <Link to="/home/elements/modele">
                     <Button className="custom-btn" startDecorator={<Garage />}>Modèle</Button>
                 </Link>
-                <Link to="/myCar/elements/categorie">
+                <Link to="/home/elements/categorie">
                     <Button className="custom-btn" startDecorator={<Commute />}>Catégorie</Button>
                 </Link>
-                <Link to="/myCar/elements/carburant">
+                <Link to="/home/elements/carburant">
                     <Button className="custom-btn" startDecorator={<EvStation />}>Carburant</Button>
                 </Link>
-                <Link to="/myCar/elements/equipement">
+                <Link to="/home/elements/equipement">
                     <Button className="custom-btn" startDecorator={<PanTool />}>Equipement</Button>
                 </Link>
-                <Link to="/myCar/elements/transmission">
+                <Link to="/home/elements/transmission">
                     <Button className="custom-btn" startDecorator={<Add />}>Transmission</Button>
                 </Link>
             </Stack>
