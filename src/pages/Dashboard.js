2d6ae46fadfe952c4666163113e183b79071be67
nohan24 <nohanrazafinjato@gmail.com>
2024-01-13 17:59:11 +0300

    chart

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,20 +3,114 @@ import Card from '@mui/joy/Card';
 import CardContent from '@mui/joy/CardContent';
 import CircularProgress from '@mui/joy/CircularProgress';
 import Typography from '@mui/joy/Typography';
-import SvgIcon from '@mui/joy/SvgIcon';
-import {Add} from '@mui/icons-material';
+import {Announcement, People, Troubleshoot, Sell} from '@mui/icons-material';
+import { BarChart } from '@mui/x-charts/BarChart';
+
 export default function Dashboard(){
+    const chartSetting = {
+
+        width: 1000,
+        height: 600,
+    };
+    const dataset = [
+        {
+            london: 59,
+            paris: 57,
+            newYork: 86,
+            seoul: 21,
+            month: 'Jan',
+        },
+        {
+            london: 50,
+            paris: 52,
+            newYork: 78,
+            seoul: 28,
+            month: 'Fév',
+        },
+        {
+            london: 47,
+            paris: 53,
+            newYork: 106,
+            seoul: 41,
+            month: 'Mar',
+        },
+        {
+            london: 54,
+            paris: 56,
+            newYork: 92,
+            seoul: 73,
+            month: 'Avr',
+        },
+        {
+            london: 57,
+            paris: 69,
+            newYork: 92,
+            seoul: 99,
+            month: 'Mai',
+        },
+        {
+            london: 60,
+            paris: 63,
+            newYork: 103,
+            seoul: 144,
+            month: 'Juin',
+        },
+        {
+            london: 59,
+            paris: 60,
+            newYork: 105,
+            seoul: 319,
+            month: 'Juil',
+        },
+        {
+            london: 65,
+            paris: 60,
+            newYork: 106,
+            seoul: 249,
+            month: 'Août',
+        },
+        {
+            london: 51,
+            paris: 51,
+            newYork: 95,
+            seoul: 131,
+            month: 'Sep',
+        },
+        {
+            london: 60,
+            paris: 65,
+            newYork: 97,
+            seoul: 55,
+            month: 'Oct',
+        },
+        {
+            london: 67,
+            paris: 64,
+            newYork: 76,
+            seoul: 48,
+            month: 'Nov',
+        },
+        {
+            london: 61,
+            paris: 70,
+            newYork: 103,
+            seoul: 25,
+            month: 'Déc',
+        },
+    ];
+
+    const valueFormatter = (value) => `${value} annonces`;
     return(
         <>
             <Grid container spacing={1} sx={{ flexGrow: 1 }}>
                 <Grid xs={12} md={4} lg={3}>
                     <Card variant="solid" invertedColors sx={{ backgroundColor: '#ff6464' }}>
                         <CardContent orientation="horizontal">
-                            <CircularProgress size="lg" determinate value={20}>
-                                <Add/>
+                            <CircularProgress size="lg" determinate value={100}>
+                                <People/>
                             </CircularProgress>
                             <CardContent>
-                                <Typography level="body-md">Utilisateur inscrit</Typography>
+                                <Typography color="white" level="body-md">Utilisateur inscrit</Typography>
                                 <Typography level="h2">1,456</Typography>
                             </CardContent>
                         </CardContent>
@@ -25,52 +119,24 @@ export default function Dashboard(){
                 <Grid xs={12} md={4} lg={3}>
                     <Card variant="solid" color="primary" invertedColors>
                         <CardContent orientation="horizontal">
-                            <CircularProgress size="lg" determinate value={20}>
-                                <SvgIcon>
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        strokeWidth={1.5}
-                                        stroke="currentColor"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941"
-                                        />
-                                    </svg>
-                                </SvgIcon>
+                            <CircularProgress size="lg" determinate value={100}>
+                                <Announcement/>
                             </CircularProgress>
                             <CardContent>
-                                <Typography level="body-md">Annonce en cours</Typography>
+                                <Typography color="white" level="body-md">Annonce en cours</Typography>
                                 <Typography level="h2">1,200</Typography>
                             </CardContent>
                         </CardContent>
                     </Card>
                 </Grid>
-                <Grid xs={6} md={4} lg={3}>
+                <Grid xs={12} md={4} lg={3}>
                     <Card variant="solid" sx={{backgroundColor:"rgba(227,175,51,0.99)"}} invertedColors>
                         <CardContent orientation="horizontal">
-                            <CircularProgress size="lg" determinate value={20}>
-                                <SvgIcon>
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        strokeWidth={1.5}
-                                        stroke="currentColor"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941"
-                                        />
-                                    </svg>
-                                </SvgIcon>
+                            <CircularProgress size="lg" determinate value={100}>
+                                <Sell/>
                             </CircularProgress>
                             <CardContent>
-                                <Typography level="body-md">Vente conclue</Typography>
+                                <Typography color="white" level="body-md">Vente conclue</Typography>
                                 <Typography level="h2">40</Typography>
                             </CardContent>
                         </CardContent>
@@ -79,29 +145,23 @@ export default function Dashboard(){
                 <Grid xs={12} md={4} lg={3}>
                     <Card variant="solid" invertedColors sx={{backgroundColor:'#4dc989'}}>
                         <CardContent orientation="horizontal">
-                            <CircularProgress size="lg" determinate value={20}>
-                                <SvgIcon>
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        strokeWidth={1.5}
-                                        stroke="currentColor"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941"
-                                        />
-                                    </svg>
-                                </SvgIcon>
+                            <CircularProgress size="lg" determinate value={100}>
+                                <Troubleshoot/>
                             </CircularProgress>
                             <CardContent>
-                                <Typography level="body-md">% de conversion d'annonce en vente</Typography>
+                                <Typography level="body-md" color="white">% d'annonce en vente</Typography>
                                 <Typography level="h2">14.45 %</Typography>
                             </CardContent>
                         </CardContent>
                     </Card>
+                </Grid>
+                <Grid xs={12}>
+                    <BarChart
+                        dataset={dataset}
+                        xAxis={[{ scaleType: 'band', dataKey: 'month' }]}
+                        series={[{ dataKey: 'seoul', label: "Annonce par mois", valueFormatter }]}
+                        {...chartSetting}
+                    />
                 </Grid>
             </Grid>
         </>
