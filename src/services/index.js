7b6c82813c672b9f83a643852e7564334d1d36f8
nohan24 <nohanrazafinjato@gmail.com>
2024-01-20 20:33:05 +0300

    update services

--- src/services/index.js
+++ src/services/index.js
@@ -1,159 +1,159 @@
 import axiosInstance from './AxionInstance'
 
 export async function login(formData){
-    return axiosInstance.post("http://localhost:8080/auth/login", formData);
+    return axiosInstance.post("https://rest-production-e2d3.up.railway.app/auth/login", formData);
 }
 
 export async function getMarques(){
-    return axiosInstance.get("http://localhost:8080/marques");
+    return axiosInstance.get("https://rest-production-e2d3.up.railway.app/marques");
 }
 
 export async function getCategories(){
-    return axiosInstance.get("http://localhost:8080/categories");
+    return axiosInstance.get("https://rest-production-e2d3.up.railway.app/categories");
 }
 
 export async function getCarburants(){
-    return axiosInstance.get("http://localhost:8080/carburants");
+    return axiosInstance.get("https://rest-production-e2d3.up.railway.app/carburants");
 }
 
 export async function getTransmissions(){
-    return axiosInstance.get("http://localhost:8080/transmissions");
+    return axiosInstance.get("https://rest-production-e2d3.up.railway.app/transmissions");
 }
 
 export async function getModeles(){
-    return axiosInstance.get("http://localhost:8080/modeles");
+    return axiosInstance.get("https://rest-production-e2d3.up.railway.app/modeles");
 }
 
 export async function getEquipements(){
-    return axiosInstance.get("http://localhost:8080/equipements");
+    return axiosInstance.get("https://rest-production-e2d3.up.railway.app/equipements");
 }
 
 export async function insertMarque(data){
     let token = localStorage.getItem("auth");
-    return axiosInstance.post("http://localhost:8080/marques", data, {
+    return axiosInstance.post("https://rest-production-e2d3.up.railway.app/marques", data, {
         headers: {"Authorization" : `Bearer ${token}`}
     });
 }
 
 export async function insertModele(data){
     let token = localStorage.getItem("auth");
-    return axiosInstance.post("http://localhost:8080/modeles", data, {
+    return axiosInstance.post("https://rest-production-e2d3.up.railway.app/modeles", data, {
         headers: {"Authorization" : `Bearer ${token}`}
     });
 }
 
 export async function insertCategorie(data){
     let token = localStorage.getItem("auth");
-    return axiosInstance.post("http://localhost:8080/categories", data, {
+    return axiosInstance.post("https://rest-production-e2d3.up.railway.app/categories", data, {
         headers: {"Authorization" : `Bearer ${token}`}
     });
 }
 
 export async function insertCarburant(data){
     let token = localStorage.getItem("auth");
-    return axiosInstance.post("http://localhost:8080/carburants", data, {
+    return axiosInstance.post("https://rest-production-e2d3.up.railway.app/carburants", data, {
         headers: {"Authorization" : `Bearer ${token}`}
     });
 }
 
 export async function insertEquipement(data){
     let token = localStorage.getItem("auth");
-    return axiosInstance.post("http://localhost:8080/equipements", data, {
+    return axiosInstance.post("https://rest-production-e2d3.up.railway.app/equipements", data, {
         headers: {"Authorization" : `Bearer ${token}`}
     });
 }
 
 export async function insertTransmission(data){
     let token = localStorage.getItem("auth");
-    return axiosInstance.post("http://localhost:8080/transmissions", data, {
+    return axiosInstance.post("https://rest-production-e2d3.up.railway.app/transmissions", data, {
         headers: {"Authorization" : `Bearer ${token}`}
     });
 }
 
 export async function deleteMarque(id){
     let token = localStorage.getItem("auth");
-    return axiosInstance.delete("http://localhost:8080/marques/" + id, {
+    return axiosInstance.delete("https://rest-production-e2d3.up.railway.app/marques/" + id, {
         headers: {"Authorization" : `Bearer ${token}`}
     });
 }
 
 export async function deleteModele(id){
     let token = localStorage.getItem("auth");
-    return axiosInstance.delete("http://localhost:8080/modeles/" + id, {
+    return axiosInstance.delete("https://rest-production-e2d3.up.railway.app/modeles/" + id, {
         headers: {"Authorization" : `Bearer ${token}`}
     });
 }
 
 export async function deleteCategorie(id){
     let token = localStorage.getItem("auth");
-    return axiosInstance.delete("http://localhost:8080/categories/"+id, {
+    return axiosInstance.delete("https://rest-production-e2d3.up.railway.app/categories/"+id, {
         headers: {"Authorization" : `Bearer ${token}`}
     });
 }
 
 export async function deleteCarburant(id){
     let token = localStorage.getItem("auth");
-    return axiosInstance.delete("http://localhost:8080/carburants/"+id, {
+    return axiosInstance.delete("https://rest-production-e2d3.up.railway.app/carburants/"+id, {
         headers: {"Authorization" : `Bearer ${token}`}
     });
 }
 
 export async function deleteEquipement(id){
     let token = localStorage.getItem("auth");
-    return axiosInstance.delete("http://localhost:8080/equipements/"+id, {
+    return axiosInstance.delete("https://rest-production-e2d3.up.railway.app/equipements/"+id, {
         headers: {"Authorization" : `Bearer ${token}`}
     });
 }
 
 export async function deleteTransmission(id){
     let token = localStorage.getItem("auth");
-    return axiosInstance.delete("http://localhost:8080/transmissions/"+id, {
+    return axiosInstance.delete("https://rest-production-e2d3.up.railway.app/transmissions/"+id, {
         headers: {"Authorization" : `Bearer ${token}`}
     });
 }
 
 export async function updateCommission(data){
     let token = localStorage.getItem("auth");
-    return axiosInstance.put("http://localhost:8080/commissions", data, {
+    return axiosInstance.put("https://rest-production-e2d3.up.railway.app/commissions", data, {
         headers: {"Authorization": `Bearer ${token}`}
     });
 }
 
 export async function getCommission(){
     let token = localStorage.getItem("auth");
-    return axiosInstance.get("http://localhost:8080/commissions", {
+    return axiosInstance.get("https://rest-production-e2d3.up.railway.app/commissions", {
         headers: {"Authorization": `Bearer ${token}`}
     });
 }
 
 export function images(path){
-    return "http://localhost:8080/images/" + path;
+    return "https://rest-production-e2d3.up.railway.app/images/" + path;
 }
 
 export async function getAnnonces(){
     let token = localStorage.getItem("auth");
-    return axiosInstance.get("http://localhost:8080/validation/annonces", {
+    return axiosInstance.get("https://rest-production-e2d3.up.railway.app/validation/annonces", {
         headers: {"Authorization": `Bearer ${token}`}
     });
 }
 
 export async function validateannonce(id){
     let token = localStorage.getItem("auth");
-    return axiosInstance.put("http://localhost:8080/validation/annonces/" + id, null,{
+    return axiosInstance.put("https://rest-production-e2d3.up.railway.app/validation/annonces/" + id, null,{
         headers: {"Authorization": `Bearer ${token}`}
     });
 }
 
 export async function refuser(id){
     let token = localStorage.getItem("auth");
-    return axiosInstance.delete("http://localhost:8080/validation/annonces/" + id,{
+    return axiosInstance.delete("https://rest-production-e2d3.up.railway.app/validation/annonces/" + id,{
         headers: {"Authorization": `Bearer ${token}`}
     });
 }
 
 export async function stat(){
     let token = localStorage.getItem("auth");
-    return axiosInstance.get("http://localhost:8080/statistiques",{
+    return axiosInstance.get("https://rest-production-e2d3.up.railway.app/statistiques",{
         headers: {"Authorization": `Bearer ${token}`}
     });
 }
